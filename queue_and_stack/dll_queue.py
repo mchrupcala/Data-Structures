0aba30cc72d4da6fb9427790f8e1fde87470bacc
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2020-01-27 16:42:30 -0500

    Added some comments with questions I have

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,6 +7,7 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
+        #   Because the pointers in a DLL give us a strict order to work with, so queues and stacks can remove/add from the head/tail pretty easily.
         # self.storage = ?
 
     def enqueue(self, value):
